6026ef5e1ed8238bb9fa70590e4421be5bc67bae
Alejandro Pardo <alejandro.pardo.r@gmail.com>
2013-07-27 22:56:40 -0430

    Twitter timeline

---
+++ app/Config/twitter.php
@@ -0,0 +1,13 @@
+<?php
+  /**
+   * Get keys from twitter and fill in this content.
+   */
+  $config = array(
+  	'Twitter' => array(
+      'consumer_key' => 'QusCalqxKtD6hn1FVQLGYg',
+      'consumer_secret' => 'HdTlyuUUCHXgdOwL9r8FDiwTgtTtIDrFpGSXoItrw0',
+      'oauth_token' => '268096605-pjcH5Fg2BVOTu03Jb9waDXEU3j8uUngzKSOPK7aA',
+      'oauth_token_secret' => '4k74FMBlors2IgaOaV7QQ6GUl8aeKy2l9v0nFrKRaOI'
+    )
+  );
+?>
